8e227d621eef292955c18250fe047dca6260f4b2
Abdullah1071 <abdullahshahid1071@gmail.com>
2023-05-17 03:09:04 +0500

    add models book, author, genre

---
+++ models/author.model.js
@@ -0,0 +1,10 @@
+
+class Author {
+    constructor(id, name, books) {
+        this.id = id;
+        this.name = name;
+        this.books = books;
+    }
+}
+
+export default Author;
---
+++ models/book.model.js
@@ -0,0 +1,12 @@
+
+class Book {
+    constructor(id, title, author, genre, price) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.price = price;
+    }
+}
+
+export default Book;
